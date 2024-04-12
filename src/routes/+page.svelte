ffe88b7886de87888b359bc7dcb03676b6f181dd
JJ <44622919+jj-stuff@users.noreply.github.com>
2024-04-12 20:55:59 +0700

    init svelte fix

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	const name = 'Steve';
+	const channel = '<b>jj._.aaa</b>';
+	const status = 'current';
+</script>
+
+<main>
+	<h1>New In</h1>
+	<h2 class={status}>Hoodie</h2>
+</main>
+
+<style>
+	@font-face {
+		font-family: 'Croisan';
+		src: url('fonts/CROISAN.otf');
+		font-weight: normal;
+		font-style: normal;
+	}
+	main {
+		font-family: '', 'Times New Roman', Times, serif;
+	}
+	h1 {
+		color: black;
+	}
+	.current {
+		text-decoration: underline;
+	}
+</style>
